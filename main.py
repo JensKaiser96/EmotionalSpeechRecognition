b16278823d028e3e9cc51988471570e879b29a73
st142204 <ask21D-3414_/!>
2023-03-07 12:25:08 +0100

    change - extended pipeline

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from src.model.models import simple_lstm, train, smart_train
+import torch
+
+from src.models.models import train, annotate, CNNModel2
 from src.data.data import get_train_loader, get_dev_loader
 
 
 def main():
-    model, optimizer = simple_lstm(0.025)
+    # model, optimizer = simple_lstm(0.025)
+    model = CNNModel2()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.025)
 
-    train(model, optimizer, get_train_loader(), get_dev_loader(), 5)
+    train(model, optimizer, get_train_loader(small=True), num_epochs=5, path="data/models/small")
+    annotate(model, get_dev_loader(), "data/dev_1.json")
 
 
 if __name__ == '__main__':
